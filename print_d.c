51636d5b7f77b68600894888e76dfdab38f8e944
Bryan Minner <bminner@w-r8-p5.s19.be>
2020-01-23 16:59:15 +0100

    tsb

--- print_d.c
+++ print_d.c
@@ -12,60 +12,6 @@
 
 #include "ft_printf.h"
 
-char	*case2(t_flag *combi, char *toprint, int cas)
-{
-	char	*dest;
-	int		i;
-	int		n;
-
-	i = 0;
-	if (cas == 1)
-	{
-		dest = ft_malloc_zero(combi->len);
-		n = combi->len - ft_strlen(toprint);
-		while (toprint[i] != '\0')
-			dest[n++] = toprint[i++];
-	}
-	else if (cas == 2)
-	{
-		dest = ft_malloc_space(combi->len);
-		n = 0;
-		dest[n++] = '-';
-		while (n <= (combi->len - ft_strlen(toprint)))
-			dest[n++] = '0';
-		while (toprint[i] != '\0')
-			dest[n++] = toprint[i++];
-	}
-	return (dest);
-}
-
-char	*case1(t_flag *combi, char *toprint, int cas)
-{
-	char	*dest;
-	int		n;
-	int		i;
-
-	i = 0;
-	if (cas == 1)
-	{
-		n = combi->precision - ft_strlen(toprint);
-		dest = ft_malloc_zero(combi->precision);
-		while (toprint[i] != '\0')
-			dest[n++] = toprint[i++];
-	}
-	else if (cas == 2)
-	{
-		dest = ft_malloc_space(combi->len);
-		n = combi->len - combi->precision;
-		dest[n - 1] = '-';
-		while (n < (combi->len - ft_strlen(toprint)))
-			dest[n++] = '0';
-		while (toprint[i] != '\0')
-			dest[n++] = toprint[i++];
-	}
-	return (dest);
-}
-
 int		ft_len(long n)
 {
 	int i;
@@ -116,22 +62,66 @@ char	*print_integer(t_flag *combi, int num)
 {
 	char	*dest;
 	char	*toprint;
+	int		i;
+	int		n;
 
+	i = 0;
 	if (num < 0)
 	{
 		combi->negatif = 1;
 		num *= -1;
 	}
 	toprint = ft_itoa(num);
-	if (combi->precision > combi->len && combi->precision > ft_strlen(toprint))
-		dest = case1(combi, toprint, 1);
-	else if (combi->len > combi->precision && combi->precision > ft_strlen(toprint) && combi->moins == 0)
-		dest = case1(combi, toprint, 2);
-	else if (combi->len > combi->precision && combi->precision > ft_strlen(toprint))
-		dest = case2(combi, toprint, 2);
-	else if (combi->len > ft_strlen(toprint) && combi->zero == 1)
-		dest = case2(combi, toprint, 1);
-	else
-		dest = toprint;
+
+	if (combi->len > ft_strlen(toprint) || combi->precision > ft_strlen(toprint))
+	{
+		if (combi->precision > combi->len && combi->precision > ft_strlen(toprint))
+		{
+			if (combi->negatif == 1)
+			{
+				dest = ft_malloc_zero(combi->precision + 1);
+				dest[0] = '-';
+				n = combi->precision - ft_strlen(toprint) + 1;
+				while (toprint[i] != '\0')
+					dest[n++] = toprint[i++];
+			}
+			else
+			{
+				dest = ft_malloc_zero(combi->precision);
+				n = combi->precision - ft_strlen(toprint);
+				while (toprint[i] != '\0')
+					dest[n++] = toprint[i++];
+			}
+		}
+		else if (combi->len > combi->precision && combi->precision > ft_strlen(toprint))
+		{
+			dest = ft_malloc_space(combi->len);
+			if (combi->moins == 1)
+			{
+				n = 0;
+				if (combi->negatif == 1)
+					dest[n++] = '-';
+				while (n <= (combi->precision - ft_strlen(toprint)))
+					dest[n++] = '0';
+				while (toprint[i] != '\0')
+					dest[n++] = toprint[i++];
+			}
+			else
+			{
+				n = combi->len - combi->precision;
+				if (combi->negatif == 1)
+					dest[n - 1] = '-';
+				while (n <= (combi->precision - ft_strlen(toprint)))
+					dest[n++] = '0';
+				while (toprint[i] != '\0')
+					dest[n++] = toprint[i++];
+			}
+		}
+		else if (combi->len > combi->precision && combi->precision != 0)
+		{
+			//appliquer le testprintf
+		}
+	}
+
 	return (dest);
 }
